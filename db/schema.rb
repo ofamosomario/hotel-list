512c342badfffac0e8fa7b8259e0fc56a1e2deee
mario <tkyakow@gmail.com>
2019-01-24 20:12:15 -0200

    Database changed for Postegres

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,20 @@
 
 ActiveRecord::Schema.define(version: 2019_01_23_000621) do
 
-  create_table "facilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "facilities", force: :cascade do |t|
+    t.string "name", limit: 255
     t.bigint "hotel_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["hotel_id"], name: "index_facilities_on_hotel_id"
   end
 
-  create_table "hotels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
+  create_table "hotels", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "location", limit: 255
     t.decimal "price", precision: 8, scale: 2
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_01_23_000621) do
     t.index ["user_id"], name: "index_hotels_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
